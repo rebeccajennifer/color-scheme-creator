e57ca615a31c7c769557d9edf7547a5fce91a066
rebecca <rebeccarashkin@gmail.com>
2025-07-06 17:21:37 -0700

    Pytest setup (#4)
    - Added pytest framework
    - Updated color scheme preview table
    - Updated flux-dark theme

---
+++ test/test_rgb_color.py
@@ -0,0 +1,48 @@
+#_______________________________________________________________________
+#       _   __   _   _ _   _   _   _         _
+#  |   |_| | _  | | | V | | | | / |_/ |_| | /
+#  |__ | | |__| |_| |   | |_| | \ |   | | | \_
+#   _  _         _ ___  _       _ ___   _                        / /
+#  /  | | |\ |  \   |  | / | | /   |   \                        (^^)
+#  \_ |_| | \| _/   |  | \ |_| \_  |  _/                        (____)o
+#_______________________________________________________________________
+#
+#-----------------------------------------------------------------------
+#  Copyright 2024, Rebecca Rashkin
+#  -------------------------------
+#  This code may be copied, redistributed, transformed, or built upon in
+#  any format for educational, non-commercial purposes.
+#
+#  Please give me appropriate credit should you choose to modify this
+#  code. Thank you :)
+#-----------------------------------------------------------------------
+#
+#_______________________________________________________________________
+#  //\^.^/\\   //\^.^/\\   //\^.^/\\   //\^.^/\\   //\^.^/\\   //\^.^/\\
+#_______________________________________________________________________
+
+#_______________________________________________________________________
+# Tests for utility functions.
+#_______________________________________________________________________
+
+from classes.rgb_color import RgbColor
+from classes.rgb_color import RgbConst
+
+#_______________________________________________________________________
+class TestConst:
+  """
+  Contains constants used in tests.
+  """
+
+  COLOR_INT: int = 0x80FF10
+  RED_VAL: int = 128
+  GRN_VAL: int = 255
+  BLU_VAL: int =  16
+
+  ERR_VAL: int = -1
+
+#_______________________________________________________________________
+def test_get_rgb_from_hex():
+  rgb_dict = RgbColor.get_rgb_from_hex(TestConst.COLOR_INT)
+
+  assert rgb_dict[RgbConst.RED_STR] == TestConst.RED_VAL
