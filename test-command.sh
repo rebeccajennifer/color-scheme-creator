8250fab38925679e2069b9b523673794a3bfca21
rebecca <rebeccarashkin@gmail.com>
2025-09-29 17:21:11 -0700

    fixed test-command

--- test-command.sh
+++ test-command.sh
@@ -1,1 +1,6 @@
-python color_scheme_driver.py --scheme_type gnome --file sample-themes/flux-dark.json
+#!/usr/bin/bash
+python3 color_scheme_driver.py                                \
+--scheme_type gnome                                           \
+--file        sample-themes/flux-dark/flux-dark-input.json    \
+--out_dir     sample-themes/flux-dark                         \
+--name        bar
