7414d4cc811f8e21d064d5f6e0c7ca1d90da4cd0
rebecca <rebeccarashkin@gmail.com>
2025-11-27 20:03:44 -0700

    Added to vscode template, removed configurations from launch.json, added lite colors to replace color labels script

--- replace_color_labels.py
+++ replace_color_labels.py
@@ -32,9 +32,12 @@ def get_color_label_map() -> dict:
   Returns a mapping of color labels to their RGB integer values.
   """
 
-  color_label_map: dict =\
+  color_label_map_dark: dict =\
   { 'FG__NORM': 'c6c6c6'
   , 'BG__NORM': '262626'
+  , 'FG__BOLD': 'afafaf'
+  , 'BG__BOLD': '303030'
+
   , 'BLK_NORM': '1c1c1c'
   , 'RED_NORM': 'd787af'
   , 'GRN_NORM': 'afd787'
@@ -43,8 +46,6 @@ def get_color_label_map() -> dict:
   , 'VIO_NORM': 'af87d7'
   , 'CYA_NORM': '87d7af'
   , 'WHT_NORM': 'd7d7d7'
-  , 'FG__BOLD': 'd7d7d7'
-  , 'BG__BOLD': '303030'
   , 'BLK_BOLD': '121212'
   , 'RED_BOLD': 'd75f87'
   , 'GRN_BOLD': '87af5f'
@@ -53,6 +54,7 @@ def get_color_label_map() -> dict:
   , 'VIO_BOLD': 'af87af'
   , 'CYA_BOLD': '5fafaf'
   , 'WHT_BOLD': 'd0d0d0'
+
   , 'FG__INVL': 'ff5f87'
   , 'BG__INVL': 'af5f87'
 
@@ -63,7 +65,40 @@ def get_color_label_map() -> dict:
   , 'ORN_BGND': 'ff875f'
   }
 
-  return color_label_map
+  color_label_map_lite: dict =\
+  { 'FG__NORM': '262626'
+  , 'BG__NORM': 'e4e4e4'
+  , 'FG__BOLD': '303030'
+  , 'BG__BOLD': 'dadada'
+
+  , 'BLK_NORM': '121212'
+  , 'RED_NORM': 'af005f'
+  , 'GRN_NORM': '008700'
+  , 'YEL_NORM': 'd75f00'
+  , 'BLU_NORM': '005f87'
+  , 'VIO_NORM': '870087'
+  , 'CYA_NORM': '008787'
+  , 'WHT_NORM': '878787'
+  , 'BLK_BOLD': '262626'
+  , 'RED_BOLD': 'd7005f'
+  , 'GRN_BOLD': '0087af'
+  , 'YEL_BOLD': 'd78700'
+  , 'BLU_BOLD': '005faf'
+  , 'VIO_BOLD': '875f87'
+  , 'CYA_BOLD': '005f5f'
+  , 'WHT_BOLD': 'afafaf'
+
+  , 'FG__INVL': 'd75f87'
+  , 'BG__INVL': 'af5f87'
+
+  , 'BLU_BGND': '00afd7'
+  , 'CYA_BGND': '87d7d7'
+  , 'VIO_BGND': 'af87d7'
+  , 'GRN_BGND': 'afd787'
+  , 'ORN_BGND': 'ff875f'
+  }
+
+  return color_label_map_dark
 
 if __name__ == '__main__':
 
